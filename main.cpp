05e7cdbf16412e04528bc9c51130eb5324033c52
tech108 <xogomo@gmail.com>
2026-09-23 14:49:41 -0600

    Add personalized greeting

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
+
+std::string greeting(const std::string& name) {
+    return "Hello, " + name + "!";
+}
 
 int main() {
-    std::cout << "Hello from Git!\n";
-    int i = 5;
-    int y = 2;
-    int j = 10;
+    std::cout << greeting("Git user") << '\n';
     return 0;
 }
